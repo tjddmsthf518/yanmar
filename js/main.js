8b9141a302783bf407a8c7ef6f588a5149dd78d7
tjddmsthf518 <tjddmsthf518@gmail.com>
2025-10-29 16:32:39 +0900

    yanmar_25_10_29_2차

--- js/main.js
+++ js/main.js
@@ -1,4 +1,39 @@
 $(document).ready(function () {
+
+  $(".menu_bar").click(function () {
+    $(".sub_menu").addClass("active");
+  });
+
+  $(".close").click(function () {
+    $(".sub_menu").removeClass("active");
+  });
+
+  var clicked = "on";
+
+  $(".product").click(function () {
+    if (clicked == "on") {
+      $(".sub_product").css({
+        display: "block",
+      });
+
+      $(".product img").css({
+        transform: "rotate(-180deg)",
+      });
+
+      clicked = "off";
+    } else if (clicked == "off") {
+      $(".sub_product").css({
+        display: "none",
+      });
+
+      $(".product img").css({
+        transform: "rotate(0deg)",
+      });
+
+      clicked = "on";
+    }
+  });
+
   var swiper = new Swiper(".con_wrap_3", {
     pagination: {
       el: ".swiper-pagination",
